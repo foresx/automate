54aa1987dd00d8d706893a510de864fbda979736
Alex Pop <apop@chef.io>
2020-08-25 19:15:49 +0100

    update tests6
    Signed-off-by: Alex Pop <apop@chef.io>

--- components/compliance-service/api/tests/03_deep_reports_spec.rb
+++ components/compliance-service/api/tests/03_deep_reports_spec.rb
@@ -23,10 +23,10 @@ describe File.basename(__FILE__) do
         Reporting::ListFilter.new(type: 'profile_id',
                                   values: ['447542ecfb8a8800ed0146039da3af8fed047f575f6037cfba75f3b664a97ea4'])
     ])
-    actual_nodes_hash = actual_nodes.to_h
-    actual_nodes_hash[:reports].each { |r| c[:end_time] = 'SOMETIME_IN_THE_LAST_24H' }
+    actual_data_hash = actual_data.to_h
+    actual_data_hash[:reports].each { |r| c[:end_time] = 'SOMETIME_IN_THE_LAST_24H' }
     expected_json = {'what':true}.to_json
-    assert_equal_json_sorted(expected_json, actual_nodes_hash.to_json)
+    assert_equal_json_sorted(expected_json, actual_data_hash.to_json)
 
     actual_data = GRPC reporting, :list_reports, Reporting::Query.new(filters: [
         Reporting::ListFilter.new(type: 'profile_id',
