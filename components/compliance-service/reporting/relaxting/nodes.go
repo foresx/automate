277331a766f990456e959a5d9ab88a4140343133
Alex Pop <apop@chef.io>
2020-08-25 16:04:24 +0100

    ignore start_time in nodes
    Signed-off-by: Alex Pop <apop@chef.io>

--- components/compliance-service/reporting/relaxting/nodes.go
+++ components/compliance-service/reporting/relaxting/nodes.go
@@ -45,6 +45,8 @@ func (backend *ES2Backend) GetNodes(from int32, size int32, filters map[string][
 	emptyTotals := TotalNodeCounts{Total: 0, Passed: 0, Skipped: 0, Failed: 0, Waived: 0}
 	myName := "GetNodes"
 
+	// Only end_time matters for this call
+	filters["start_time"] = filters["end_time"]
 	depth, err := backend.NewDepth(filters, true)
 	if err != nil {
 		return nil, emptyTotals, errors.Wrap(err, fmt.Sprintf("%s unable to get depth level for report", myName))
